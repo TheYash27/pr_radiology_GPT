32b02ce6224fe62f303432bc9a55c5a27474605b
unknown <f20201429@goa.bits-pilani.ac.in>
2023-08-03 15:35:30 +0530

    3 Aug 6

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ async function fetchReply() {
                 temperature: 0.37,
                 presence_penalty: 0.37,
                 frequency_penalty: -0.37,
-                max_tokens: 500
+                max_tokens: 70
             })
             AIResponse = response.data.choices[0].message.content.replace(/#/g, "") 
             push(conversationInDb, {
